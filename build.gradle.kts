1336682717d6c9f3c79da8a277f7972746e528b8
Eva Tatarka <eva@tatarka.me>
2023-03-05 23:20:43 -0800

    Set up nexus publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,14 @@ plugins {
     id("com.android.library") version "7.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("androidx.benchmark") version "1.1.1" apply false
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+}
+
+group = "me.tatarka.android"
+version = "0.1"
+
+nexusPublishing {
+    repositories {
+        sonatype()
+    }
 }
